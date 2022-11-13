a78969e933d77377284c5c4952f529b44ba05de2
marley <mmm@marley-2.local>
2022-11-13 12:10:09 +0000

    first commit

---
+++ Syntax.py
@@ -0,0 +1,72 @@
+# Python Indentation
+# Indentation refers to the spaces at the beginning of 
+# a code line.
+
+# Where in other programming languages the indentation 
+# in code is for readability only, the indentation in Python is very important.
+# Python uses indentation to indicate a block of code
+
+
+ 
+
+
+# Python Comments
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+
+
+# Creating a Comment
+# Comments starts with a #, and Python will ignore them:
+
+
+
+
+
+"""Multi Line Comments
+Python does not really have a syntax for multi line comments.
+
+To add a multiline comment you could insert a # for each line:
+Or, not quite as intended, you can use a multiline string.
+
+Since Python will ignore string literals that are not assigned to a variable, 
+you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+""" """
+"""
+
+
+
+2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,19 @@
+"""
+Variables
+Variables are containers for storing data values.
+
+"""
+
+
+# variableName = value
+
+# myname = "kofi"
+
+# Hello world
+
+# camel case --> helloWorld  for variableName
+# snake case  --> hello_world for function
+# Pascal case --> HelloWorld for class
+ 
+_fullname,age = "kofi",40
+print(age)
